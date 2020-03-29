c320f35e3eeca27dbce368399024880e881864b7
Eiden0 <shibashis12@gmail.com>
2020-03-29 21:39:10 +0530

    Update table.jsx
    added the value column function

--- src/Components/Table/table.jsx
+++ src/Components/Table/table.jsx
@@ -34,20 +34,30 @@ class Table extends Component{
     render() {
         console.log('render table 1');
         const value = function (v) {
-            if(v === '>'){
+            if(v.conditionValue === '>'){
                 return(
                     <th><input type="number" id=">" name="greaterThan"/></th>
                 )
             }
-            if(v === '<'){
-                return(<th>less than</th>)
+            else if(v.conditionValue === '<'){
+                return(<th><input type="number" id="<" name="lessThan"/></th>)
             }
-            if(v === '='){
-                return(<th>equal to</th>)
+            else if(v.conditionValue === '='){
+                return(<th><input type="number" id="=" name="equalTo"/></th>)
             }
-            if(v === '><'){
-                return(<th>between</th>)
+            else if(v.conditionValue === '><'){
+                return(
+                    <th>
+                        <label htmlFor="min">Minimum:</label>
+                        <input type="number" id="min" name="min"/>
+                        <span>
+                        <label htmlFor="max">Maximum:</label>
+                        <input type="number" id="max" name="max"/>
+                        </span>
+                    </th>
+                )
             }
+            else return null
         };
         if(this.props.option !== null) {
             console.log('render table 2');
@@ -58,7 +68,7 @@ class Table extends Component{
                             <tr>
                                 <th>{this.props.option}</th>
                                 <th><Select options={options} onChange={this.handleChange} /></th>
-                                {value(this.state.conditionValue)}
+                                {value(this.state)}
                             </tr>
                             </tbody>
                         </table>
